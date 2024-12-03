6ebdcb408ac9c7b1266be7e98c0b305154caf420
ivanxalie <ivanxalie@gmail.com>
2024-12-03 21:32:56 +0300

    Add a few tests

--- src/test/java/guru/springframework/sdjpaintro/SpringBootJpaSliceTest.java
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpaSliceTest.java
@@ -1,7 +1,11 @@
 package guru.springframework.sdjpaintro;
 
+import guru.springframework.sdjpaintro.domain.AuthorUuid;
 import guru.springframework.sdjpaintro.domain.Book;
+import guru.springframework.sdjpaintro.domain.BookUuid;
+import guru.springframework.sdjpaintro.repository.AuthorUuidRepository;
 import guru.springframework.sdjpaintro.repository.BookRepository;
+import guru.springframework.sdjpaintro.repository.BookUuidRepository;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -19,6 +23,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SpringBootJpaSliceTest {
     @Autowired
     BookRepository repository;
+    @Autowired
+    BookUuidRepository bookUuidRepository;
+    @Autowired
+    AuthorUuidRepository authorUuidRepository;
 
     @Test
     @Order(10)
@@ -44,5 +52,40 @@ public class SpringBootJpaSliceTest {
         long countBefore = repository.count();
 
         assertThat(countBefore).isEqualTo(3);
+    }
+
+    @Test
+    void testBookUuidSave() {
+        String title = "How can I help?";
+        BookUuid saved = bookUuidRepository.save(BookUuid.builder()
+                .title(title)
+                .build());
+
+        assertThat(bookUuidRepository.count()).isGreaterThan(1);
+
+        assertThat(bookUuidRepository.findById(saved.getId()))
+                .isNotNull()
+                .satisfies(bookUuid -> {
+                    assertThat(bookUuid.orElseThrow().getTitle()).isEqualTo(title);
+                });
+    }
+
+    @Test
+    void testAuthorUuidSave() {
+        String firstName = "Alex";
+        String lastName = "Green";
+        AuthorUuid saved = authorUuidRepository.save(AuthorUuid.builder()
+                .firstName(firstName)
+                .lastName(lastName)
+                .build());
+
+        assertThat(authorUuidRepository.count()).isGreaterThan(1);
+
+        assertThat(authorUuidRepository.findById(saved.getId()))
+                .isNotNull()
+                .satisfies(authorUuid -> {
+                    assertThat(authorUuid.orElseThrow().getFirstName()).isEqualTo(firstName);
+                    assertThat(authorUuid.orElseThrow().getLastName()).isEqualTo(lastName);
+                });
     }
 }
