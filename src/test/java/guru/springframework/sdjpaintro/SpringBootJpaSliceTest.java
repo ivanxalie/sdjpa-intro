bdcdc0a0df7850f5424cc14d22d27ac9418d2286
ivanxalie <ivanxalie@gmail.com>
2024-12-03 19:42:50 +0300

    Fixed SpringBootJpaSliceTest

--- src/test/java/guru/springframework/sdjpaintro/SpringBootJpaSliceTest.java
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpaSliceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.Commit;
@@ -17,7 +16,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @ComponentScan(basePackages = {"guru.springframework.sdjpaintro.bootstrap"})
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class SpringBootJpaSliceTest {
     @Autowired
     BookRepository repository;
